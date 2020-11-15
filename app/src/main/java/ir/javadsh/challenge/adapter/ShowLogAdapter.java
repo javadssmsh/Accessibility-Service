c9927b4ab145e1406c3769e54e9c8a77dd0058cb
javad <j.shirkhani@clickhub.ir>
2020-11-16 02:47:09 +0330

    constructor added to adapter

--- app/src/main/java/ir/javadsh/challenge/adapter/ShowLogAdapter.java
+++ app/src/main/java/ir/javadsh/challenge/adapter/ShowLogAdapter.java
@@ -1,18 +1,35 @@
 package ir.javadsh.challenge.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.zip.Inflater;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import ir.javadsh.challenge.R;
+import ir.javadsh.challenge.model.Log;
 
 public class ShowLogAdapter extends RecyclerView.Adapter<ShowLogAdapter.LogViewHolder> {
 
+    private Context context;
+    private List<Log> logList;
+
+    public ShowLogAdapter(Context context, List<Log> logList) {
+        this.context = context;
+        this.logList = logList;
+    }
 
     @NonNull
     @Override
     public LogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_log, parent, false);
+        return new LogViewHolder(view);
     }
 
     @Override
@@ -22,12 +39,23 @@ public class ShowLogAdapter extends RecyclerView.Adapter<ShowLogAdapter.LogViewH
 
     @Override
     public int getItemCount() {
-        return 0;
+        return logList.size();
     }
 
     public class LogViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView urlTv;
+        TextView createdDateTv;
+        TextView browserNameTv;
+
         public LogViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageView = itemView.findViewById(R.id.imgUrl);
+            urlTv = itemView.findViewById(R.id.url);
+            createdDateTv = itemView.findViewById(R.id.created_date);
+            browserNameTv = itemView.findViewById(R.id.browser_name);
+
         }
     }
 }
